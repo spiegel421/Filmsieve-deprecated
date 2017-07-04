4e93d44e4e3523dc77cc80d3eac2c86e5e68f927
spiegel421 <spiegel421@gmail.com>
2017-07-04 02:19:49 -0400

    Update and rename readtags.py to execute.py

--- backend/execute.py
+++ backend/execute.py
@@ -1,10 +1,10 @@
-from movietags import update_movie_tags
+from movietags import *
 
-def read_tags(filename):
+def read_movietags(filename):
   reader = open(filename, mode='r')
   line = reader.readline()
   while line != '':
     datum = line.split(',')
-    movietags.update_movie_tags(datum[0], datum[1], datum[2])
+    update_movie_tags(datum[0], datum[1], datum[2])
     line = reader.readline()
   reader.close()
