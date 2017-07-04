5d6e59b770b2e51daaf49d29329b43a4fbb3b54b
spiegel421 <spiegel421@gmail.com>
2017-07-04 02:18:47 -0400

    Update readtags.py

--- backend/readtags.py
+++ backend/readtags.py
@@ -1,1 +1,10 @@
+from movietags import update_movie_tags
 
+def read_tags(filename):
+  reader = open(filename, mode='r')
+  line = reader.readline()
+  while line != '':
+    datum = line.split(',')
+    movietags.update_movie_tags(datum[0], datum[1], datum[2])
+    line = reader.readline()
+  reader.close()
