2f2b65f0635848dcb5b96c539cc10b66ef8d7e11
spiegel421 <spiegel421@gmail.com>
2017-07-05 18:08:41 -0400

    Create useritemtag.py

---
+++ backend/useritemtag.py
@@ -0,0 +1,102 @@
+# Stores a table for movie tags.
+import mysql.connector
+from mysql.connector import errorcode
+
+DB_NAME = 'useritemtag'
+
+TABLES = {}
+# Every time a user tags a movie, the user, movie, and tag are stored.
+TABLES['movie_tags'] = (
+  "CREATE TABLE album_tags( "
+  "user varchar(20) NOT NULL, "
+  "item varchar(100) NOT NULL, "
+  "tag varchar(20) NOT NULL ); ")
+
+# Connects to mysql, defines a method for creating the database, and uses that method.
+cnx = mysql.connector.connect(user='root', password='Reverie42')
+cursor = cnx.cursor()
+
+def create_database(cursor):
+  try:
+    cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+  except mysql.connector.Error as err:
+     print "Failed creating database: {}".format(err)
+     exit(1)
+
+try:
+    cnx.database = DB_NAME  
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        cnx.database = DB_NAME
+    else:
+        print err
+        exit(1)
+
+for name, ddl in TABLES.iteritems():
+    try:
+        print "Creating table {}: ".format(name)
+        cursor.execute(ddl)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print "already exists."
+        else:
+            print err.msg
+    else:
+        print "OK"
+
+cnx.commit()
+cursor.close()
+cnx.close()
+
+# Allows users to update either tag table.
+def update_tags(tablename, user, item, tag):
+  cnx = mysql.connector.connect(user='root', password='Reverie42', buffered=True)
+  cursor = cnx.cursor()
+  cnx.database = DB_NAME
+  
+  check_exists = ("SELECT EXISTS(SELECT 1 FROM "
+                  "%s WHERE user = %s "
+                  "AND item = %s AND tag = %s); ")
+  
+  add_tag = ("INSERT INTO %s "
+             "(user, item, tag) "
+             "VALUES (%s, %s, %s); ")
+  
+  data = (tablename, user, item, tag)
+  
+  cursor.execute(check_exists, data)
+  for i in cursor:
+    exists = i[0]
+  if exists == 0:
+    cursor.execute(add_tag, data)
+  
+  cnx.commit()
+  cursor.close()
+  cnx.close()
+  
+# Reads either tag table into a dictionary.
+def read_tags(tablename):
+  cnx = mysql.connector.connect(user='root', password='Reverie42', buffered=True)
+  cursor = cnx.cursor()
+  cnx.database = DB_NAME
+  
+  tag_dict = {}
+  
+  cursor.execute("SELECT * FROM %s; " % (tablename))
+  for i in cursor:
+    if i[1] in tag_dict:
+      if i[2] in tag_dict[i[1]]:
+        tag_dict[i[1]][i[2]] += 1
+      else:
+        tag_dict[i[1]][i[2]] = 1
+    else:
+      tag_dict[i[1]] = {}
+      tag_dict[i[1]][i[2]] = 1
+  
+  cnx.commit()
+  cursor.close()
+  cnx.close()
+  
+  return tag_dict
