6301802eaaddd34fef269432b83eaae6f45d3d5d
spiegel421 <spiegel421@gmail.com>
2017-07-05 18:09:38 -0400

    Update execute.py

--- backend/execute.py
+++ backend/execute.py
@@ -1,10 +1,10 @@
-from movietags import *
+from useritemtag import *
 
-def read_movietags(filename):
+def read_tags(filename):
   reader = open(filename, mode='r')
   line = reader.readline()
   while line != '':
     datum = line.split(',')
-    update_movie_tags(datum[0], datum[1], datum[2])
+    update_tags(datum[0], datum[1], datum[2])
     line = reader.readline()
   reader.close()
