76ffe5b12c8001d7f1d4fb8b05e20e91216db0cb
spiegel421 <spiegel421@gmail.com>
2017-07-05 18:45:45 -0400

    Update movietag.py

--- backend/movietag.py
+++ backend/movietag.py
@@ -6,12 +6,12 @@ DB_NAME = 'movietag'
 TABLES = {}
 TABLES['movie_tags'] = (
   "CREATE TABLE movie_tags( "
-  "movie varchar(100) NOT NULL, "
-  "tag varchar(20) NOT NULL ); ")
+  "movie int(10) NOT NULL, "
+  "tag varchar(100) NOT NULL ); ")
 
 TABLES['tags'] = (
   "CREATE TABLE tags( "
-  "tag varchar(20) NOT NULL ); ")
+  "tag varchar(100) NOT NULL ); ")
 
 cnx = mysql.connector.connect(user='root', password='Reverie42')
 cursor = cnx.cursor()
